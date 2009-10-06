558b0f1f8d4c6f4703dca4127867ee331c72b4b7
Kuba Z <ja.zajko@gmail.com>
2009-10-06 22:15:36 +0200

    pierwszy commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   main.cpp
+ * Author: szef
+ *
+ * Created on October 6, 2009, 10:13 PM
+ */
+
+#include <stdlib.h>
+#include <iostream>
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout<<"Hello, fucks !";
+    return (EXIT_SUCCESS);
+}
+
